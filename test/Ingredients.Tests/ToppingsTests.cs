0f08fb49e91bec074f883d02ac3382365ff8802a
Mark Rendle <mark@markrendle.net>
2021-03-23 11:05:41 +0000

    Add ToppingsTests

--- test/Ingredients.Tests/ToppingsTests.cs
+++ test/Ingredients.Tests/ToppingsTests.cs
@@ -1,14 +1,31 @@
 using System;
+using System.Threading.Tasks;
+using Ingredients.Protos;
+using TestHelpers;
 using Xunit;
 
 namespace Ingredients.Tests
 {
-    public class ToppingsTests
+    public class ToppingsTests : IClassFixture<IngredientsApplicationFactory>
     {
-        [Fact]
-        public void Test1()
-        {
+        private IngredientsApplicationFactory _factory;
 
+        public ToppingsTests(IngredientsApplicationFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetsToppings()
+        {
+            var client = _factory.CreateGrpcClient();
+
+            var response = await client.GetToppingsAsync(new GetToppingsRequest());
+            
+            Assert.Collection(response.Toppings,
+                t => Assert.Equal("cheese", t.Topping.Id),
+                t => Assert.Equal("tomato", t.Topping.Id)
+                );
         }
     }
 }
