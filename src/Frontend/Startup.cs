656dff47e38743be5b0ce843fcb2af01abc9b6cc
Mark Rendle <mark@markrendle.net>
2021-03-23 10:04:01 +0000

    Added Ingredients service

--- src/Frontend/Startup.cs
+++ src/Frontend/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ingredients.Protos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,10 @@ namespace Frontend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddGrpcClient<IngredientsService.IngredientsServiceClient>(options =>
+            {
+                options.Address = new Uri("https://localhost:5003");
+            });
             services.AddControllersWithViews();
         }
 
---
+++ src/Ingredients/Services/IngredientsService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Ingredients.Protos;
+using Microsoft.Extensions.Logging;
+using Pizza.Data;
+
+namespace Ingredients
+{
+    internal class IngredientsService : Protos.IngredientsService.IngredientsServiceBase
+    {
+        private readonly IToppingData _toppingData;
+        private readonly ILogger<IngredientsService> _logger;
+
+        public IngredientsService(IToppingData toppingData, ILogger<IngredientsService> logger)
+        {
+            _toppingData = toppingData;
+            _logger = logger;
+        }
+
+        public override async Task<GetToppingsResponse> GetToppings(GetToppingsRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var toppings = await _toppingData.GetAsync(context.CancellationToken);
+                var availableToppings = toppings.Select(t =>
+                    new AvailableTopping
+                    {
+                        Quantity = t.StockCount,
+                        Topping = new Topping
+                        {
+                            Id = t.Id,
+                            Name = t.Name,
+                            Price = (double) t.Price
+                        }
+                    });
+
+                var response = new GetToppingsResponse
+                {
+                    Toppings = {availableToppings}
+                };
+                return response;
+            }
+            catch (OperationCanceledException)
+            {
+                _logger.LogWarning("Operation was cancelled.");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
